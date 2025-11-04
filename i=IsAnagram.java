1084d951b66a68fe4daf46f3676efc21dcbc1910
Anthony <90572842+Anth1337@users.noreply.github.com>
2025-11-04 00:12:16 -0500

    added NC problem
    isAnagram, O(s+t) time, O(s+t) space

---
+++ i=IsAnagram.java
@@ -0,0 +1,23 @@
+class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            countS.put(cs, countS.getOrDefault(cs, 0) + 1);
+            countT.put(ct, countT.getOrDefault(ct, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int a = countS.getOrDefault(c, 0);
+            int b = countT.getOrDefault(c, 0);
+            if (a != b) return false;
+        }
+        return true;
+    }
+}
